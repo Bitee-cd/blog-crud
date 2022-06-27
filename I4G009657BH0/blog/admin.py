84bc194225668c1403c180b2b6e28988befab307
Bitee-cd <calebduniya45@gmail.com>
2022-06-27 16:10:05 +0100

    completion

---
+++ I4G009657BH0/blog/admin.py
@@ -0,0 +1,25 @@
+from dataclasses import fields
+from django.contrib import admin
+
+from .models import Post
+# Register your models here.
+
+
+class PostAdmin(admin.PostAdmin):
+
+    class Meta:
+        model = Post
+
+        fields = [
+            'title',
+            'slug',
+            'author',
+            'body',
+            'publish',
+            'created',
+            'updated',
+            'status',
+        ]
+
+
+admin.site.register(Post, PostAdmin)
